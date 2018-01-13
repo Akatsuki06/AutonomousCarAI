fb048f00bbb5c7978af32a78a95b9dfa2440a205
suraz <suraznegi@gmail.com>
2018-01-13 23:23:09 +0530

    updated model

--- model.py
+++ model.py
@@ -38,15 +38,17 @@ num_classes = 4
 epochs = 20
 model = Sequential()
  
-model.add(Convolution2D(32, kernel_size=(5, 5), activation='relu',
-                        input_shape=input_shape))
+model.add(Convolution2D(32, kernel_size=(5, 5), activation='relu',input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Convolution2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Convolution2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
-#model.add(Convolution2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
+model.add(Dense(128, activation='relu'))
 model.add(Dense(4, activation='softmax'))
 
 
